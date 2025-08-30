caf385c43023d32584f5d356f5df346281877736
Arthur Alekseev <arthur100500@gmail.com>
2025-08-30 07:30:26 +0300

    [fix] Fixed security (role checks), add @TestPropertySource annotation
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- approximations-common/src/main/java/generated/org/springframework/boot/resolvers/PathVariableMethodArgumentResolverImpl.java
+++ approximations-common/src/main/java/generated/org/springframework/boot/resolvers/PathVariableMethodArgumentResolverImpl.java
@@ -2,18 +2,20 @@ package generated.org.springframework.boot.resolvers;
 
 import generated.org.springframework.boot.SymbolicValueFactory;
 import generated.org.springframework.boot.pinnedValues.PinnedValueSource;
+import generated.org.springframework.boot.pinnedValues.PinnedValueStorage;
 import org.jacodb.approximation.annotation.Approximate;
 import org.springframework.core.MethodParameter;
 import org.springframework.lang.Nullable;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.PathVariableMethodArgumentResolver;
+import org.usvm.spring.api.SpringEngine;
 
 @Approximate(PathVariableMethodArgumentResolver.class)
 public class PathVariableMethodArgumentResolverImpl {
     @Nullable
     protected Object resolveName(String name, MethodParameter parameter, NativeWebRequest request) throws Exception {
         String parameterName = parameter.getParameterName();
-        String path = request.getContextPath();
+        String path = PinnedValueStorage.getPinnedValue(PinnedValueSource.REQUEST_PATH, String.class);
         if (parameterName == null)
             return null;
 
